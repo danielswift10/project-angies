e87a3adf9b93e9cedc6c4185d1056324167c42a2
danielswift10 <ojeyomi.daniel@gmail.com>
2023-07-09 20:35:40 +0100

    feat: prepared folder structure

---
+++ src/Assets/index.js
@@ -0,0 +1,52 @@
+//ICONS
+import arrowRight from "./icons/arrow-right.png"
+import earth from "./icons/earth.png"
+import entertain from "./icons/entertaining.svg"
+import flexibility from "./icons/flexibility.svg"
+import hamburger from "./icons/hamburger.svg"
+import instructor from "./icons/instructor.svg"
+import logo from "./icons/logo.svg"
+import personalized from "./icons/personalized.svg"
+import quote1 from "./icons/quote-1.png"
+import quote2 from "./icons/quote-2.png"
+import quote3 from "./icons/quote-3.png"
+import reward from "./icons/reward.svg"
+import collaborative from "./icons/collaborative.svg"
+
+
+//IMAGES
+import bgMb from "./images/bg-mb.png"
+import bgLg from "./images/bg-lg.png"
+import brand from "./images/brand.png"
+import dywan from "./images/dywan.png"
+import felix from "./images/felix.png"
+import introImage from "./images/intro-image-mb.png"
+import paris from "./images/paris.png"
+import collaborateLg from "./images/collaborate-lg.png"
+import collaborateMb from "./images/collaborate-mb.png"
+
+
+export {
+    arrowRight,
+    earth,
+    entertain,
+    flexibility,
+    hamburger,
+    instructor,
+    logo,
+    personalized,
+    quote1,
+    quote2,
+    quote3,
+    reward,
+    collaborative,
+    bgLg,
+    bgMb,
+    brand,
+    dywan,
+    felix,
+    introImage,
+    paris,
+    collaborateLg,
+    collaborateMb,
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#272D3D",
+        purple: "#2E2BA6",
+        pale: "#8887AE",
+        pink: "#D23C8F",
+        yellow: "#F2B04D",
+        white: "#FFFFFF",
+        secondary: "#F9F9FA",
+        veryDimBlue: "#8A89A0",
+
+        
+
+
+
+
+      },
+      fontFamily: {
+        poppins: ["Changa One", "PT Sans"],
+      },
+      fontWeight: {
+        normal: 400,
+        bold: 700,
+      },
+      boxShadow: {
+        "yellowBtn": "0px 23px 48px 0px #f2b04d66",
+        "purpleBtn": "0px 23px 48px 0px #2e2ba666",
+      }
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      smd: "880px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1440px",
+    },
   },
   plugins: [],
-}
-
+};
